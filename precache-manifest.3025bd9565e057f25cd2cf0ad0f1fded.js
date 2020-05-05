cdd81a47737dff41a2564edb25cdb793928cd2cb
iamawebgeek <iamawebgeek@gmail.com>
2020-05-05 15:47:58 +0500

    Updates

---
+++ precache-manifest.3025bd9565e057f25cd2cf0ad0f1fded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d73597a99d01241a98539a324501125",
+    "url": "/react-bubble-loading/index.html"
+  },
+  {
+    "revision": "36b322653864453ecabc",
+    "url": "/react-bubble-loading/static/css/main.aec8f6db.chunk.css"
+  },
+  {
+    "revision": "d24c0dead60e44634067",
+    "url": "/react-bubble-loading/static/js/2.3aef89f0.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/react-bubble-loading/static/js/2.3aef89f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b322653864453ecabc",
+    "url": "/react-bubble-loading/static/js/main.f223c1d6.chunk.js"
+  },
+  {
+    "revision": "f3fe7c66b599cb8d38f3",
+    "url": "/react-bubble-loading/static/js/runtime-main.00f76bb8.js"
+  }
+]);
